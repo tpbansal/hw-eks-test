f00faf60e5d60866d6202227fab7852da204a245
tpbansal <tapasvi.bansal@lego.com>
2021-11-29 09:57:31 +0100

    network_layer

---
+++ infra/main.tf
@@ -0,0 +1,80 @@
+locals {
+  cluster_name = "dribing-test-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# VPC, SUBNETS
+# ---------------------------------------------------------------------------------------------------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "v3.6.0"
+
+  name                 = "dribing-test-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+    name_prefix = "worker_group_mgmt_one"
+    vpc_id = module.vpc.vpc_id
+    description = "Allow internal SSH traffic"
+ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+cidr_blocks = [
+            "10.0.0.0/8"
+        ]
+    }
+}
+resource "aws_security_group" "worker_group_mgmt_two" {
+    name_prefix = "worker_group_mgmt_two"
+    vpc_id = module.vpc.vpc_id
+    description = "Allow internal SSH traffic"
+ 
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+cidr_blocks = [
+            "10.0.0.0/8"
+        ]
+    }
+}
+resource "aws_security_group" "all_worker_mgmt" {
+    name_prefix = "all_worker_management"
+    vpc_id = module.vpc.vpc_id
+    description = "Allow https traffic"
+ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+cidr_blocks = [
+            "10.0.0.0/8"
+        ]
+    }
+}
